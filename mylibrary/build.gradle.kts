6c81b750d0d98b741674122b272c062a657d8b04
Nasrin Khan <nasrin2000khan2000@gmail.com>
2025-12-14 19:17:46 +0430

    initial commit

---
+++ mylibrary/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.khan.mylibrary"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    publishing {
+        singleVariant("release")  // <-- use double quotes, NOT single quotes
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ mylibrary/src/main/java/com/khan/mylibrary/myClass.java
@@ -0,0 +1,10 @@
+package com.khan.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class myClass {
+    public static void xyz(Context context){
+        Toast.makeText(context, "Hey bro", Toast.LENGTH_SHORT).show();
+    }
+}
